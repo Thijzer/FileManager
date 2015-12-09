29e654cbc8e8017b74f26835410fe70b4bb602d0
Thijzer <thijs.dp@gmail.com>
2015-12-09 21:39:05 +0100

    File : added comments, removed some properties

--- src/File.php
+++ src/File.php
@@ -1,116 +1,181 @@
 <?php
 
-class File extends SPLFileInfo
+/*
+ * This file is part of the File Manager.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace src;
+
+/**
+ * @author Thijs De Paepe <thijs.dp@gmail.com>
+ */
+class File extends \SPLFileInfo
 {
+    /*
+     * @var string
+     */
     private $fullPath;
     private $content;
-    private $filesizeFormat;
-    private $mimetype;
 
-    public $filesize;
-    public $filename;
-
-    public function __construct($fullPath, $content = null, $properties = null)
+    /**
+     * Construct
+     *
+     * @param string $fullPath the destination Path of a file
+     * @param string $content  setup the File content
+     */
+    public function __construct($fullPath, $content = null)
     {
         parent::__construct($fullPath);
 
-        if (is_array($properties)) {
-            foreach ($properties as $key => $value) {
-                $this->{$key} = $value;
-            }
-        }
-        $this->filename = $this->getFilename();
         $this->fullPath = $fullPath;
         $this->content = $content;
     }
 
-    public function exists()
-    {
-        return $this->isfile();
-    }
-
+    /**
+     * Returns the File's Full Path
+     *
+     * @return string
+     */
     public function getFullPath()
     {
         return $this->fullPath;
     }
 
+    /**
+     * Return the CRC32b hash created of the full path
+     * easier for checksums or matches
+     *
+     * @return string CRC32b hash
+     */
     public function getFullPathHash()
     {
-        return crc32b($this->fullPath);
+        return $this->crc32b($this->fullPath);
     }
 
+    /**
+     * Get File Content
+     *
+     * @return string file content
+     */
     public function getContent()
     {
         return (!$this->content) ? @file_get_contents($this->fullPath) : $this->content;
     }
 
+    /**
+     * Set File Content
+     *
+     * @param string $content
+     */
     public function setContent($content)
     {
         $this->content = $content;
         return $this;
     }
 
+    /**
+     * Returns the md5 hash of the File's content
+     *
+     * @return string hash of file content
+     */
     public function getHash()
     {
-        return crc32b($this->getContent());
+        return md5($this->getContent());
     }
 
+    /**
+     * Returns the mime Type of the File
+     *
+     * @return [type] [description]
+     */
     public function getMimeType()
     {
-        if (!$this->mimetype && $this->exsists()) {
-            $finfo = finfo_open(FILEINFO_MIME_TYPE);
-            $this->mimetype = finfo_file($finfo, $this->fullPath);
-            finfo_close($finfo);
+        if (!$this->isfile()) {
+            throw new \Exception("Error Processing Request : File content doesn't exist");
         }
-        return $this->mimetype;
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $mimetype = finfo_file($finfo, $this->fullPath);
+        finfo_close($finfo);
+        return $mimetype;
     }
 
+    /**
+     * Returns the byte size of the file
+     *
+     * @return string
+     */
     public function getSizeInBytes()
     {
-        if (!$this->exists()) {
-            return; # File is still in memory
+        if (!$this->isfile()) {
+            throw new \Exception("Error Processing Request : File content doesn't exist");
         }
-        if (!$this->filesize) {
-            clearstatcache(); # required
-            $this->filesize = filesize($this->fullPath);
-        }
-        return $this->filesize;
+        clearstatcache(); # required
+        $bytesize = filesize($this->fullPath);
+        return $bytesize;
     }
 
+    /**
+     * Returns the Directory the file is stored in
+     *
+     * @return string
+     */
     public function getDirectory()
     {
         return pathinfo($this->fullPath, PATHINFO_DIRNAME);
     }
 
+    /**
+     * Returns the Formatted File size of the File
+     *
+     * @param  integer $decimals the number of decimals you wish to return
+     * @return string  Formatted Filesize
+     */
     public function getFilesize($decimals = 2)
     {
-        if (!$this->filesizeFormat) {
-            $bytes = $this->getSizeInBytes();
-            $size = array("Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB");
-            $factor = floor((strlen($bytes) - 1) / 3);
-            $this->filesizeFormat = sprintf("%.{$decimals}f", $bytes / pow(1024, $factor));
-            $this->filesizeFormat .= ' ' . @$size[$factor];
-        }
-        return $this->filesizeFormat;
+        $bytes = $this->getSizeInBytes();
+        $size = array("Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB");
+        $factor = floor((strlen($bytes) - 1) / 3);
+        $formattedFilesize = sprintf("%.{$decimals}f", $bytes / pow(1024, $factor));
+        $formattedFilesize .= ' ' . @$size[$factor];
+        return $formattedFilesize;
     }
 
-    public function remove($filename)
+    /** Delete */
+    public function delete()
     {
         $this->content = null;
+        // we still need to remove the file
         unlink($this->fullPath);
-        return null;
     }
 
+    /**
+     * Save Content
+     *
+     * @throws Exception throw if the file has no Content
+     */
     public function save()
     {
-        if (!empty($this->content)) {
-            try {
-                $localFile = fopen($this->fullPath, 'w+');
-                fwrite($localFile, $this->getContent());
-                fclose($localFile);
-            } catch (Exception $e) {
-                die($e);
-            }
+        try {
+            $localFile = fopen($this->fullPath, 'w+');
+            fwrite($localFile, $this->getContent());
+            fclose($localFile);
+        } catch (\Exception $e) {
+            throw new \Exception("Error Processing Request", $e);
         }
+    }
+
+    /**
+     * 8 character long hash
+     * ideal for filename checksums
+     *
+     * @param  string $value
+     * @return string
+     */
+    private function crc32b($value)
+    {
+        return hash('crc32b', $value);
     }
 }
